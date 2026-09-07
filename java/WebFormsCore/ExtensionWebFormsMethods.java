630e5c221118a74a70abfdae0a9051b25fe357b5
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2026-09-07 23:25:27 +0330

    Update ExtensionWebFormsMethods.java

--- java/WebFormsCore/ExtensionWebFormsMethods.java
+++ java/WebFormsCore/ExtensionWebFormsMethods.java
@@ -1,58 +1,60 @@
-package WebFormsCore;
+// WebForms.java 2.1 - The Back-End Part of WebForms Core Technology, Owned by Elanat (https://elanat.net)
+// Compatible with WebFormsJS version 2.1
 
-// WebForms.java 2.0 - The Back-End Part of WebForms Core Technology, Owned by Elanat (https://elanat.net)
-// Compatible with WebFormsJS version 2.0
+package webformscore;
 
 public class ExtensionWebFormsMethods {
-    /// <summary>
-    /// This Method Does Not Support QueryAll
-    /// </summary>
-    public static String appendPlace(String text, String value) {
-        if (text == null || text.length() < 1)
+    public static String child(String text, String value) {
+        if (text == null || text.length() < 1) {
             return value;
-
+        }
         return text + "|" + value;
     }
 
-    public static String appendParent(String text) {
-        return "/" + text;
-    }
-
-    public static String exportActionControlsToWebFormsTag(String actionControls, boolean addLine) {
-        return (addLine ? "\n" : "") + "<web-forms ac=\"" + actionControls + "\"></web-forms>";
-    }
-
-    public static String exportActionControlsToWebFormsTag(String actionControls) {
-        return exportActionControlsToWebFormsTag(actionControls, false);
-    }
-
-    public static String exportActionControlsToHtmlComment(String actionControls, boolean addLine) {
-        return (addLine ? "\n" : "") + "<!--[web-forms]\n" + actionControls + "-->";
-    }
-
-    public static String exportActionControlsToHtmlComment(String actionControls) {
-        return exportActionControlsToHtmlComment(actionControls, false);
-    }
-
-    public static String exportActionControlsToResponse(String actionControls) {
-        return "[web-forms]\n" + actionControls;
-    }
-
-    public static String removeOuter(String text, String startString, String endString) {
-        int start = text.indexOf(startString);
-        if (start == -1)
+    public static String parent(String text) {
+        if (text == null || text.length() < 1) {
             return text;
+        }
+        if (text.endsWith("|/") || text.endsWith("//")) {
+            return text + '/';
+        }
+        return text + "|/";
+    }
 
-        int end = text.indexOf(endString, start);
-        if (end == -1)
-            return text;
+    public static String criteria(String text, String value) {
+        if (text == null || text.length() < 1) {
+            return value;
+        }
+        return text + "?" + value.replace("|", "$[vb];").replace("?", "$[qu];");
+    }
 
-        int lengthToRemove = (end - start) + endString.length();
+    public static String appendFetchReplace(String text, String searchValue, String value) {
+        char fs = (char) 28;
+        text = text.length() > 0 ? text.substring(1) : "";
+        return "@;" + searchValue + fs + value + fs + text;
+    }
 
-        return text.substring(0, start) + text.substring(start + lengthToRemove);
+    public static String lineBreak(String text, boolean encodeLine) {
+        String encode = encodeLine ? "$[sln];" : "";
+        return text.replace("\r\n", encode).replace("\n", encode).replace("\r", encode);
     }
 
     public static String lineBreak(String text) {
-        return text.replace("\n", "$[sln]");
+        return lineBreak(text, false);
+    }
+
+    // Converts Numbers to Strings
+    public static String toJsString(String text) {
+        return "\"" + text + "\"";
+    }
+
+    // Get JS Object Momentary 
+    public static String toJsObject(String text) {
+        return "$" + text;
+    }
+
+    // Get JS Object Returned Value Once
+    public static String toJsReturnObject(String text) {
+        return "$@" + text;
     }
 }
