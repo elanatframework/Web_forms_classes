6b76b55db29b399f9c7762ab088243e8750d3f63
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2026-09-07 23:27:57 +0330

    Update Security.java

--- java/WebFormsCore/Security.java
+++ java/WebFormsCore/Security.java
@@ -1,22 +1,22 @@
-package WebFormsCore;
+// WebForms.java 2.1 - The Back-End Part of WebForms Core Technology, Owned by Elanat (https://elanat.net)
+// Compatible with WebFormsJS version 2.1
 
-// WebForms.java 2.0 - The Back-End Part of WebForms Core Technology, Owned by Elanat (https://elanat.net)
-// Compatible with WebFormsJS version 2.0
+package webformscore;
 
 public class Security {
     public String safeValue(String value) {
-        if (value == null || value.length() < 1)
+        if (value == null || value.length() < 1) {
             return value;
-
-        if (value.charAt(0) == '@') {
-            value = value.substring(1);
-            value = "$[at];" + value;
         }
-
-        value = value.replace("\n", "$[ln];");
-        value = value.replace("|", "$[vb];");
-        value = value.replace(",@", "$[co];@");
-
+        if (value.charAt(0) == '@') {
+            value = "@" + value;
+        }
+        value = value.replace("\n", "$[ln];")
+                .replace(",@", "$[co];@")
+                .replace(String.valueOf((char) 28), "\0")
+                .replace(String.valueOf((char) 29), "\0")
+                .replace(String.valueOf((char) 30), "\0")
+                .replace(String.valueOf((char) 31), "\0");
         return value;
     }
 }
