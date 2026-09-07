c7c5ac04f1242e148aad4f29361041397d348183
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2026-09-07 23:27:45 +0330

    Update OutputPlace.java

--- java/WebFormsCore/OutputPlace.java
+++ java/WebFormsCore/OutputPlace.java
@@ -1,8 +1,7 @@
-package WebFormsCore;
+// WebForms.java 2.1 - The Back-End Part of WebForms Core Technology, Owned by Elanat (https://elanat.net)
+// Compatible with WebFormsJS version 2.1
 
-// WebForms.java 2.0 - The Back-End Part of WebForms Core Technology, Owned by Elanat (https://elanat.net)
-// Compatible with WebFormsJS version 2.0
+package webformscore;
 
 public class OutputPlace extends InputPlace {
-    // OutputPlace inherits all methods from InputPlace
 }
