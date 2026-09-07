7ca96342b234429673ed74f33b573a0a03d2ab02
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2026-09-07 23:28:09 +0330

    Update WasmLanguage.java

--- java/WebFormsCore/WasmLanguage.java
+++ java/WebFormsCore/WasmLanguage.java
@@ -1,14 +1,17 @@
-package WebFormsCore;
+// WebForms.java 2.1 - The Back-End Part of WebForms Core Technology, Owned by Elanat (https://elanat.net)
+// Compatible with WebFormsJS version 2.1
 
-// WebForms.java 2.0 - The Back-End Part of WebForms Core Technology, Owned by Elanat (https://elanat.net)
-// Compatible with WebFormsJS version 2.0
+package webformscore;
 
 public class WasmLanguage {
+    // The Suffix "Mediator" Means You Must Call the JavaScript Interface. In Other Cases, the WASM File Should Be Called Directly.
     public static final String C = "c";
     public static final String CPP = "c";
-    public static final String Rust = "rust";
-    public static final String CSharp = "csharp";
+    public static final String RUST = "rust";
+    public static final String C_SHARP = "csharp";
+    // .NET WebCIL Container. The "dotnet.js" File Should Be Invoked.
+    public static final String C_SHARP_MEDIATOR = "csharp-m";
     public static final String GO = "go";
     public static final String JAVA = "java";
-    public static final String AssemblyScript = "as";
+    public static final String ASSEMBLY_SCRIPT = "as";
 }
